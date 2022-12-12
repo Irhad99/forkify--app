de8d20a69cf6b6e574a7990cf36429ef23562b0a
Irhad99 <radacairhad99@gmail.com>
2022-12-12 01:39:24 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f46f3981-f6b2-42c9-bdd2-2317799b9454';
+export const MODAL_CLOSE_SEC = 2.5;
